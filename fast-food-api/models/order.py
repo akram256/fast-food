48b223cddf5a492f4f6a419238bd3cccc503a930
Akram Mukasa <mukasaakram55@gmail.com>
2018-09-27 18:32:54 +0300

    change order file

--- fast-food-api/models/order.py
+++ fast-food-api/models/order.py
@@ -6,7 +6,8 @@ class Order():
         Class defines the order atributes
         params: user_name, order_id, order&nbsp;&nbsp;&nbsp;&nbsp;
         """
-    def __init__(self, order_id, user_name, quantity,order):
+   
+    def __init__(self,order_id, user_name, quantity,order):
         self.order_id = order_id
         self.user_name = user_name
         self.quantity = quantity
